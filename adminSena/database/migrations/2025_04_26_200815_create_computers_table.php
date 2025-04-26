622ba966d2840c4a9289f10c298364770b2b4fc0
Leeann <diegolyt29@gmail.com>
2025-04-26 16:14:01 -0500

    Trabajo 2 V2.0

--- adminSena/database/migrations/2025_04_26_200815_create_computers_table.php
+++ adminSena/database/migrations/2025_04_26_200815_create_computers_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('computers', function (Blueprint $table) {
             $table->id();
             $table->string('number');
-            $table->string('brand');
+            $table->string('brancd');
             $table->timestamps();
         });
     }
--- adminSena/database/migrations/2025_04_26_205615_create_course_teachers_table.php
+++ adminSena/database/migrations/2025_04_26_205615_create_course_teachers_table.php
@@ -11,10 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('teachers', function (Blueprint $table) {
+        Schema::create('course_teachers', function (Blueprint $table) {
             $table->id();
-            $table ->string('name');
-            $table ->string('email')->unique();
+
+            $table->unsignedBigInteger('course_id');
+            $table->unsignedBigInteger('teacher_id');
+
+            $table->foreign('course_id')->references('id')->on('courses');
+            $table->foreign('teacher_id')->references('id')->on('teachers');
             $table->timestamps();
         });
     }
@@ -24,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('teachers');
+        Schema::dropIfExists('course_teachers');
     }
 };
